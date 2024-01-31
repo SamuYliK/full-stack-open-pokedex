2d2330d4206be5ccf079e8cd2da7f651c10d3e93
SamuYliK <samu.ylikuivila@gmail.com>
2024-01-31 23:33:17 +0200

    Added another cypress e2e tests

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,7 +1,26 @@
 describe('Pokedex', function() {
-  it('front page can be opened', function() {
+  beforeEach(function () {
     cy.visit('http://localhost:5000')
+  })
+
+  it('front page can be opened', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('pokemon page can be navigated to', function() {
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+    cy.contains('overgrow')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('back to home page button works from pokemon page', function() {
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+    cy.contains('overgrow')
+    cy.contains('Home').click()
+    cy.contains('charizard')
+    cy.contains('blastoise')
   })
 })
