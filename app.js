60481d569378a9bfe61ffaf58e3aa392a5eaea3d
SamuYliK <samu.ylikuivila@gmail.com>
2024-02-04 17:13:11 +0200

    Modified code to fail health check, #notag

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const app = express()
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
-app.use(express.static('dist'))
+app.use(express.static('dist_error'))
 
 app.get('/version', (req, res) => {
-  res.send('21 - Once merged') // change this string to ensure a new version deployed
+  res.send('22 - Should not work') // change this string to ensure a new version deployed
 })
 
 app.get('/health', (req, res) => {
@@ -15,7 +15,8 @@ app.get('/health', (req, res) => {
   res.send('ok')
 })
 
-app.listen(PORT, () => {
+// App started in wrong port, to make it unfunctional
+app.listen(PORT + 1, () => {
   /* eslint-disable-next-line no-console*/
   console.log(`server started on port ${PORT}`)
 })
