83c854b2798137920b1897746769cd1e0e0a9da6
SamuYliK <samu.ylikuivila@gmail.com>
2024-02-04 18:25:54 +0200

    Fixed wrong port of app.js, fly.toml & CI pipeline

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const app = express()
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
-app.use(express.static('dist_error'))
+app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('22 - Should not work') // change this string to ensure a new version deployed
+  res.send('24 - After merging') // change this string to ensure a new version deployed
 })
 
 app.get('/health', (req, res) => {
@@ -15,8 +15,7 @@ app.get('/health', (req, res) => {
   res.send('ok')
 })
 
-// App started in wrong port, to make it unfunctional
-app.listen(PORT + 1, () => {
+app.listen(PORT, () => {
   /* eslint-disable-next-line no-console*/
   console.log(`server started on port ${PORT}`)
 })
